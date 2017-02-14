82439e8ee057fcaf37a4b0701c88bbfedc5503e1
sinlessdays <sinlessdays@gmail.com>
2017-02-14 17:39:46 +1100

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   serverURL: "https://uber619.herokuapp.com/parse",
   databaseURI: databaseUri || 'mongodb://heroku_2j9lph57:9e5manc52h8s4kc81e8o5h8gmg@ds149049.mlab.com:49049/heroku_2j9lph57',
+  publicServerURL: process.env.PARSE_PUBLIC_SERVER_URL || 'https://uber619.herokuapp.com/parse',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'uber123ljfdsakj324czdfe',
   masterKey: process.env.MASTER_KEY || 'kajsfkjwekqrj34324dc' //Add your master key here. Keep it secret!
